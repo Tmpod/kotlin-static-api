7b56ab142e84d3dabdb3b7911925c4156706494f
tmpod <tom@tmpod.dev>
2026-05-17 20:45:18 +0100

    Bump version to 0.3-SNAPSHOT

--- buildSrc/src/main/kotlin/Meta.kt
+++ buildSrc/src/main/kotlin/Meta.kt
@@ -1,7 +1,7 @@
 object Meta {
     const val name = "static-api"
     const val group = "dev.tmpod"
-    const val version = "0.2"
+    const val version = "0.3-SNAPSHOT"
 
     val isSnapshot: Boolean get() = version.endsWith("-SNAPSHOT")
     /** Whether this library is currently private. Used in Maven uploads. */
